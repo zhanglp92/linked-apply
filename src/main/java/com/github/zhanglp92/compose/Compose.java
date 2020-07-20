132cfdb068c7303e45beb18078cf2749c0e35151
张利鹏 <zhanglipeng@imdada.cn>
2020-07-20 19:06:13 +0800

    core

---
+++ src/main/java/com/github/zhanglp92/compose/Compose.java
@@ -0,0 +1,6 @@
+package com.github.zhanglp92.compose;
+
+import java.util.HashMap;
+
+public class Compose extends HashMap<String, Object> {
+}
---
+++ src/main/java/com/github/zhanglp92/context/Context.java
@@ -0,0 +1,6 @@
+package com.github.zhanglp92.context;
+
+import com.github.zhanglp92.compose.Compose;
+
+final public class Context extends Compose {
+}
